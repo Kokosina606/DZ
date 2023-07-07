5bd2d56da5f0d28741a74337c2230dd12e874daf
Kokosina606 <138290381+Kokosina606@users.noreply.github.com>
2023-07-07 23:36:36 +0300

    Add files via upload
    Задание 1

---
+++ zad1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите два числа: ");
+
+    int a=Convert.ToInt32(Console.ReadLine());
+    int b=Convert.ToInt32(Console.ReadLine());
+        if (a>b)
+            Console.WriteLine("max " + a); 
+        else if (a==b)
+            Console.WriteLine("egual");
+        else
+            Console.WriteLine("max " + b);
