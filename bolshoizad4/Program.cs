5f8e933329a52acfb50e8e768dd0145985cf22c2
Kokosina606 <138290381+Kokosina606@users.noreply.github.com>
2023-07-07 23:38:44 +0300

    Add files via upload
    Задание 4

---
+++ bolshoizad4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+
+    int N =Convert.ToInt32(Console.ReadLine());
+    int i =1;
+
+      Console.WriteLine("Чётные числа от 1 до " + N + ":");
+    
+    while (i <= N)
+    {
+      if (i % 2 !=1)
+      {Console.WriteLine(i + ",");
+      i++;}
+      else
+      i++;
+    }
