79fd20d9cca3064e0a5c21913b2013479e2d1c01
Kokosina606 <138290381+Kokosina606@users.noreply.github.com>
2023-07-07 23:38:10 +0300

    Add files via upload
    Задание 3

---
+++ zad3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+ 
+int a=Convert.ToInt32(Console.ReadLine());
+    if (a % 2==1)
+        Console.WriteLine("No");      
+    else
+        Console.WriteLine("Yes");
